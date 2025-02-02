e3b742ee7102c6e9815181d89f9a27540857e486
Suhail <suhail@gmail.com>
2025-02-02 23:46:51 +0530

    push code to main branch

--- src/main/java/com/api/gateway/filter/JwtAuthFilter.java
+++ src/main/java/com/api/gateway/filter/JwtAuthFilter.java
@@ -1,4 +1,37 @@
 package com.api.gateway.filter;
 
-public class JwtAuthFilter {
+import com.api.gateway.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class JwtAuthFilter  implements GlobalFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String token = extractToken(exchange.getRequest());
+
+        if(token == null || !jwtUtil.validateToken(token)){
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    private String extractToken(ServerHttpRequest request){
+        String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        return (authHeader != null && authHeader.startsWith("BEARER ")) ? authHeader.substring(7) : null;
+    }
 }
--- src/main/java/com/api/gateway/util/JwtUtil.java
+++ src/main/java/com/api/gateway/util/JwtUtil.java
@@ -1,19 +1,69 @@
 package com.api.gateway.util;
 
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
+@Component
 public class JwtUtil {
-    private static final Key key = Keys.secretKeyFor(SignatureAlgorithmm.HS256);
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final String SECRET_KEY = java.util.Base64.getEncoder().encodeToString(key.getEncoded());
 
-    public static String generateToken(String username) {
-        return ServerHttpSecurity.OAuth2ResourceServerSpec.JwtSpec.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1-day validity
-                .signWith(key)
+    // Generate JWT Token
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours validity
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
+    }
+
+    // Validate Token
+    public boolean validateToken(String token, String username) {
+        final String tokenUsername = extractUsername(token);
+        return (username.equals(tokenUsername) && !isTokenExpired(token));
+    }
+
+    public boolean validateToken(String token) {
+        return !isTokenExpired(token);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 }
